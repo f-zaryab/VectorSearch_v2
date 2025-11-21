b2604eca33b70926224c4b4c0c26b295a0878627
f-zaryab <zaryabfarooqui@gmail.com>
2025-11-21 23:28:46 +0000

    repo setup

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+// import { connectDB } from "../database/db";
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+// Middlewares only --------------------------------------------------
+app.use(json());
+
+// Middlewares and Routes --------------------------------------------
+// app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/admin", adminRoute);
+// app.use("/api/v1/songs", songRoute);
+// app.use("/api/v1/albums", albumRoute);
+// app.use("/api/v1/stats", statsRoute);
+
+// API-Testing --------------------------------------------------------
+app.get("/api/v1", (req, res) => {
+  res.send("<h1>Welcome to Backend of Vector-Search Engine</h1>");
+});
+
+// Server -------------------------------------------------------------
+app.listen(PORT, () => {
+  console.log(`App is listening on Port: ${PORT}`);
+//   connectDB();
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Vector Search based Finder Engine
+
+## Running Application
+
+- For Development: npm run dev
+- Testing BE: Goto localhost:5000/api/v1 for welcome msg
+
+### Libraries used
+
+- [Express](https://expressjs.com/en/starter/installing.html): npm i express
+- [Dot-Env](https://www.npmjs.com/package/dotenv): npm i dotenv
+- [Nodemon](https://www.npmjs.com/package/nodemon): npm i nodemon
+- [Mongoose](https://mongoosejs.com/docs/): npm install mongoose
